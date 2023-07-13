58d2c914c004421f53c3400737ae03efb9a6245f
Benjamin Levicar <bl8491@student.uni-lj.si>
2023-07-14 01:15:00 +0200

    Spremenil obliko bloka in zdruzil podatke v slovar

--- izlusci_podatke.py
+++ izlusci_podatke.py
@@ -1,26 +1,26 @@
 import re
 
-with open('stran68054.html', encoding='utf-8') as dat:
-    besedilo = dat.read()
-    tekme = []
-    for najdba in re.finditer(
-        '<a class="details-link" href="/event/\d+/\w+">(?P<tekma>\w*)</a>                    </td>',
-        besedilo,             
-    ):
-        print(najdba['tekma'])
-        
+#with open('stran68054.html', encoding='utf-8') as dat:
+#    besedilo = dat.read()
+#    tekme = []
+#    for najdba in re.finditer(
+#        '<td class="date">(?P<datum>\d*\s\w*\s\d*)</td>',
+#        besedilo,             
+#    ):
+#        tekme.append(najdba['datum'])
+#print(tekme)        
 
 #bloki = []
 #with open('stran68054.html', encoding='utf-8') as dat:
 #    besedilo = dat.read()
 #    vzorec = re.compile(
-#        '<td class="date">' '.*?' '\s+</tr>',
+#        '<tr class="body">' '.*?' '\s+</tr>',
 #        flags=re.DOTALL,
 #    ) 
 #    with open('podatki.html', 'a', encoding='utf-8') as dat:
 #        for najdeno in vzorec.finditer(besedilo):
 #            dat.write(besedilo[najdeno.start(): najdeno.end()])
-#
+
 #print(bloki)
         
 #print(tekme)
@@ -45,30 +45,45 @@ with open('stran68054.html', encoding='utf-8') as dat:
 #print(len(mesto))
 
 
-
 def poisci_bloke(tekst):
     bloki = []
-    vzorec = re.compile(
-        '<td class="date">' '.*?' '\s+</tr>',
-        flags=re.DOTALL,
-    ) 
+    with open(tekst, encoding='utf-8') as dat:
+        besedilo = dat.read()
+        vzorec = re.compile(
+            '<tr class="body">' '.*?' '\s+</tr>',
+            flags=re.DOTALL,
+            )
+        for najdba in vzorec.finditer(besedilo):
+            bloki.append(besedilo[najdba.start() : najdba.end()])
 
-    for najdba in vzorec_bloka.finditer(besedilo):
-        bloki.append(besedilo[najdba.start() : najdba.end()])
+        return bloki
 
-    return bloki
+#print(poisci_bloke('stran68054.html'))
 
 
-#with open('podatki.html', encoding='utf-8') as dat:
-#def izlusci_podatke(blok):
-#    tekovalec = {}
-#    
-#    vzorec_datum = re.compile('<td class="date">(?P<datum>\d*\s\w*\s\d*)</td>')
-#    najdba_datum = vzorec_datum.search(blok)
-#    tekmovalec['datum'] = najdba_datum['datum']
-#    
-#    vzorec_rezultat = re.compile('<td class="result">(?P<rezultat>\d+)</td>')
-#    najdba_rezultat = vzorec_rezultat.search(blok)
-#    tekmovalec['rezultat'] = najdba_rezultat['rezultat']
-#    
-#    vzorec_dogodek = re.compile('<a class="details-link" href="/event/\d*/\w*">(?P<tekma>\w*)</a>')
+def izlusci_podatke(blok):
+    tekmovalec = {}
+    
+    vzorec_datum = re.compile('<td class="date">(?P<datum>\d*\s\w*\s\d*)</td>')
+    najdba_datum = vzorec_datum.search(blok)
+    tekmovalec['datum'] = najdba_datum['datum']
+    #if najdba_datum['datum'] is None:
+    #    tekmovalec['datum'] = r'/'
+    #else:
+    
+    vzorec_rezultat = re.compile('<td class="result">(?P<rezultat>\d+)</td>')
+    najdba_rezultat = vzorec_rezultat.search(blok)
+    tekmovalec['rezultat'] = najdba_rezultat['rezultat']
+    
+    vzorec_dogodek = re.compile('<a class="details-link" href="/event/\d+/\w+">(?P<dogodek>(\w+\s*)+)</a>')
+    najdba_dogodek = vzorec_dogodek.search(blok)
+    tekmovalec['dogodek'] = najdba_dogodek['dogodek']
+    
+    return tekmovalec
+
+besedilo = poisci_bloke('stran68054.html')
+for blok in besedilo:
+    #i = 1
+    #if i >= len(tekme):
+    #    break 
+    print(izlusci_podatke(blok))
